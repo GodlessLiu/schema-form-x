014351217be9e6982c1c9465e809e5f0ec0b0bb4
Hilary liu <2788370451@qq.com>
2025-07-28 00:40:30 +0800

    feat: 添加更多i18n资源

--- src/components/header/XHeader.vue
+++ src/components/header/XHeader.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <div class="mt-[8px] flex justify-between">
     <a href="/" class="flex items-center">
       <img src="/favicon-256x256.ico" alt="logo" title="SchemaFormX" class="h-10 rounded-xl mr-2">
       <span class="mr-2 font-bold text-2xl">SchemaFormX</span>
-      <small class="align-bottom">表单设计器</small>
+      <small class="align-bottom">{{ t('app.title') }}</small>
     </a>
     <div>
       <x-settings />
