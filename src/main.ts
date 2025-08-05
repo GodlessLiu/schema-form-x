c089df3cca730bb0caa3d1d3a890e3cb9634b67b
Hilary liu <2788370451@qq.com>
2025-08-05 21:46:29 +0800

    fix: 删除前端自动更新

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,11 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
-import { autoRefresh } from './lib/auto-update'
 import '~/styles/tailwind.css'
 
 function init() {
   const app = createApp(App)
-  // Enable auto-update check in production environment
-  if (import.meta.env.MODE === 'production') {
-    autoRefresh()
-  }
+
   Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
     .forEach(i => i.install?.({ app }))
   app.mount('#app')
